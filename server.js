b9deb737432490196301732ef2cebdeca3e2d524
Manpreet Kaur <mkrandhawa71@gmail.com>
2024-04-02 17:09:06 +0100

    Project files

---
+++ server.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const app = require("./app");
+
+//Error handling for uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! Shutting down ...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
+dotenv.config({ path: "./config.env" });
+//CONNECTING TO ATLAS DB
+const DB = process.env.DATABASE;
+
+mongoose
+  .connect(DB)
+  .then(() => console.log("DB connection successful!"))
+  .catch((err) => console.error("DB connection error: ", err));
+
+//Defining the port number
+const port = 3000 || process.env.PORT;
+
+//Starting the sever
+const server = app.listen(3000, "localhost", () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+//Error handling for unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLES REJECTION! Shutting down...");
+  server.close(() => {
+    process.exit(1); //aboring all pending requests/promises
+  });
+});
